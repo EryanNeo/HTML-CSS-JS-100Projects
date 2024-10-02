d3e9ce4ea7be5c2ea2799ec5f294aeaa937fe66f
EryanNeo <aryansomu123@gmail.com>
2024-10-02 14:02:21 +0530

    41-added

---
+++ 41-randomImages/app.js
@@ -0,0 +1,19 @@
+console.log('script added');
+
+const container=document.querySelector('.content');
+const baseUrl='https://source.unsplash.com/random/';
+const rows=7;
+
+for(let i=0;i<rows*3;i++){
+    const img=document.createElement('img');
+    img.src=`${baseUrl}${randomSize()}`;
+    container.appendChild(img);
+}
+
+function randomSize(){
+    return `${randomNumber()}x${randomNumber()}`
+}
+function randomNumber(){
+    return Math.floor(Math.random()*10)+300;
+}
+
